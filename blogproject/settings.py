41d3cde1073c64c3aa8677499068cb8ddf20d954
eunjin <dmswlskim@likelion.org>
2019-03-06 01:06:31 +0900

    second

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -139,3 +139,7 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID=1
 LOGIN_REDIRECT_URL='/'
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
